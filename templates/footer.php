d6d593a121b3a5a09456b82f315fdbcb2af0f97f
IM UIDev3 <uidev7@i2ivision.com>
2015-05-02 16:19:09 +0200

    Coding footer section with responsive

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,16 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer>
+	<div class="container">
+		<div class="row">
+			<?php dynamic_sidebar('sidebar-footer'); ?>
+			<div class="col-md-3">
+				<h3><?php _e('Keep in touch','LLLG'); ?></h3>
+				<p><?php _e('Our social media channel','LLLG'); ?></p>
+			</div>
+			<div class="col-md-3">
+				<h3><?php _e('Designed & Developed','LLLG'); ?></h3>
+				<a href="http://www.i2ivision.com<"><img class="img-responsive" alt="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" title="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" src="<?= get_template_directory_uri(); ?>/dist/images/i2ivision-logo.png"></a>
+				<a href="http://www.i2ivision.com<">http://www.i2ivision.com</a>
+			</div>
+		</div>
+	</div>
 </footer>
