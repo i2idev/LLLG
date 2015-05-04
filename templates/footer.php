de5631fd930b803d8dc6320d1827fd6293897e3e
IM UIDev3 <uidev7@i2ivision.com>
2015-05-04 17:26:38 +0200

    style social media section

--- templates/footer.php
+++ templates/footer.php
@@ -3,10 +3,6 @@
 		<div class="row">
 			<?php dynamic_sidebar('sidebar-footer'); ?>
 			<div class="col-md-3">
-				<h3><?php _e('Keep in touch','LLLG'); ?></h3>
-				<p><?php _e('Our social media channel','LLLG'); ?></p>
-			</div>
-			<div class="col-md-3">
 				<h3><?php _e('Designed & Developed','LLLG'); ?></h3>
 				<a href="http://www.i2ivision.com<"><img class="img-responsive" alt="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" title="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" src="<?= get_template_directory_uri(); ?>/dist/images/i2ivision-logo.png"></a>
 				<a href="http://www.i2ivision.com<">http://www.i2ivision.com</a>
