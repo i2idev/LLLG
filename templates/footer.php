88e3016402397016eb66e4b9662ca71cbf5f82b1
IM UIDev3 <uidev7@i2ivision.com>
2015-05-05 17:05:08 +0200

    add end of wrapper div

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,14 @@
+</div> <!-- END wrapper -->
 <footer class="footer">
 	<div class="container">
 		<div class="row">
 			<?php dynamic_sidebar('sidebar-footer'); ?>
 			<div class="col-md-3">
 				<h3><?php _e('Designed & Developed','LLLG'); ?></h3>
-				<a href="http://www.i2ivision.com<"><img class="img-responsive" alt="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" title="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" src="<?= get_template_directory_uri(); ?>/dist/images/i2ivision-logo.png"></a>
-				<a href="http://www.i2ivision.com<">http://www.i2ivision.com</a>
+				<a href="http://www.i2ivision.com" target="_blank">
+				<img class="img-responsive" alt="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" title="<?php _e('Designed & Developed By I2IVision.Com','LLLG'); ?>" src="<?= get_template_directory_uri(); ?>/dist/images/i2ivision-logo.png">
+				http://www.i2ivision.com
+				</a>
 			</div>
 		</div>
 	</div>
